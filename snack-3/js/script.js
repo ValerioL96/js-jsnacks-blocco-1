b997f0be3a336d3f315ffb2ea9cb469b5bf16c69
ValerioL96 <v.lembo1396@gmail.com>
2024-05-13 12:34:42 +0200

    Added Snack-3

---
+++ snack-3/js/script.js
@@ -0,0 +1,19 @@
+//Crea un array vuoto.
+
+const array[];
+
+//Chiedi per 6 volte all'utente di inserire un numero,
+const  number = Number.parseInt(prompt('Type number'), 10);
+for(number=0; number<=6; numbe++){
+
+    //se è dispari inseriscilo nell'array.
+    if(number%2 == 1){
+        console.log(array);
+    }
+    else{
+        console.log('undefined');
+    }
+}
+
+
+ 
