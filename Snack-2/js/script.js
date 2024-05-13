396441e283c5b84b06a7c8bd819e9d3598ae15c5
ValerioL96 <v.lembo1396@gmail.com>
2024-05-13 12:12:05 +0200

    Added invited list

---
+++ Snack-2/js/script.js
@@ -0,0 +1,18 @@
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+const invitati = [
+    "Francesco",
+    "Roberto",
+    "Giggetta",
+    "Francesco",
+    "Roberto",
+    "Giggetta",
+    "Francesco",
+    "Roberto",
+    "Giggetta",
+    "Francesco",
+    "Roberto",
+    "Giggetta"
+];
+console.log(invitati);
+
+ //chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
